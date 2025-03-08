4106e68c87eee93ca5bfa77af668c5a3f1563fbe
advin <adithpsycho@gmail.com>
2025-03-08 13:17:16 +0530

    envs

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, OnInit } from '@angular/core';
 import { catchError, map, Observable, retry, Subject, throwError } from 'rxjs';
 import { Todo } from '../interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,7 @@ import { Todo } from '../interface';
 export class TasksService implements OnInit {
   private http = inject(HttpClient);
   public tasks = new Subject<Todo[]>();
+  private apiUrl = environment.apiUrl
   // public tasksDemo:Todo[] = []
 
   constructor() {}
@@ -17,18 +19,18 @@ export class TasksService implements OnInit {
 
 
   getTodos() {
-    return this.http.get('http://localhost:3000/taskroute/v1').pipe(retry(2),map((res:any) => res.tasks),catchError(err => throwError(()=>err)))
+    return this.http.get(this.apiUrl).pipe(retry(2),map((res:any) => res.tasks),catchError(err => throwError(()=>err)))
   }
 
   updateTodo(id:number,data:{completed:boolean}){
-    return this.http.put(`http://localhost:3000/taskroute/v1/${id}`,data)
+    return this.http.put(`${this.apiUrl}/${id}`,data)
   }
 
   addTodo(todo:Todo){
-    return this.http.post('http://localhost:3000/taskroute/v1',todo)
+    return this.http.post(this.apiUrl,todo)
   }
 
   deleteTodo(id:number){
-    return this.http.delete(`http://localhost:3000/taskroute/v1/${id}`)
+    return this.http.delete(`${this.apiUrl}/${id}`)
   }
 }
