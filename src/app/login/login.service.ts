3834fbf2eebacf0e30830aac5e11391b5bd9d53f
advin <adithpsycho@gmail.com>
2025-03-09 21:04:29 +0530

    login service improvements

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,20 +1,32 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, retry, tap, throwError } from 'rxjs';
 import { UserLogin } from '../interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
+  private apiUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  public isLoggedIn = new BehaviorSubject(false);
 
-  private apiUrl = environment.apiUrl
-  private http = inject(HttpClient)
+  constructor() {}
 
-  constructor() { }
-
-  onLogin(userLogin:UserLogin):Observable<any>{
-    return this.http.post(`${this.apiUrl}/login`,userLogin)
+  onLogin(userLogin: UserLogin): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, userLogin).pipe(
+      retry(2),
+      tap((res:any) => {
+        if (res && res.token) {
+          this.isLoggedIn.next(true);
+          localStorage.setItem('token',res.token)
+        }
+      }),
+      catchError(err => {
+        console.error(err)
+        return throwError(()=> new Error('Login failed'))
+      })
+    );
   }
 }
